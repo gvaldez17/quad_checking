f0abb1648d4be99cc963bed3bc14e9faad037c58
Glenda Valdez <gvaldez@wisc.edu>
2015-06-11 12:12:57 -0500

    second attempt at making a function to convert gml into R

--- gmltoR.R
+++ gmltoR.R
@@ -7,8 +7,11 @@ layername <- ogrListLayers("data/Adair/Adair.gml")
 #assign shapefile/gml to a variable called adair using readOGR. dsn is location 
 adair <- readOGR(dsn= "data/Adair/Adair.gml", layer= layername[1])
 #make a function to convert gml into R and can return a data frame
-checkgml <- function(y){
-  require(rgdal)
+checkgml <- function(x){
+  png(file= paste("quad_checking/figures/", x, ".png", sep=""))
   adair <- readOGR(dsn= filename, layer= layername[1])
-  dev.off()
+  plot(x[,c('x', 'y')], col = 'red', pch=19, cex = 0.5, main = x)
+  trash <- dev.off()
 }
+
+
