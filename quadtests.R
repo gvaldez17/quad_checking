bc9da2a206b035f603a7b8ecfd512fbacc2d70be
Glenda Valdez <gvaldez@wisc.edu>
2015-06-11 17:38:58 -0500

    line 7 test if two trees have same diameters *line 9 test if diameter of all trees are < 1

--- quadtests.R
+++ quadtests.R
@@ -4,3 +4,7 @@ data.frame(az.test = adair@data$az1 > 90, diam.test = is.na(adair@data$diam1) & 
 data.frame(val.test = adair@data$species1 > 0 & adair@data$species2 > 0)
 #test to see if tree one has no value (0) while tree two has a value
 data.frame(dist.test = adair@data$dist1==0 & adair@data$dist2 > 0)
+#test to see if two trees have same data..how to write: do any two trees have the same data all across the board?
+data.frame(twin.test = adair@data$diam1 == adair@data$diam2)
+#test to see if a tree hass too small of a diameter (what would too small be?). is 1 okay to use?
+data.frame(small.test = adair@data$diam1 < 1 & adair@data$diam2 < 1 & adair@data$diam3 < 1 & adair@data$diam4 < 1)
