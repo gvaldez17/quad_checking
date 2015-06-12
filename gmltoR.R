8354509387125d17e7518cf3097545da518e5bf9
Glenda Valdez <gvaldez@wisc.edu>
2015-06-11 19:23:45 -0500

    function with one test inside it

--- gmltoR.R
+++ gmltoR.R
@@ -8,10 +8,22 @@ layername <- ogrListLayers("data/Adair/Adair.gml")
 adair <- readOGR(dsn= "data/Adair/Adair.gml", layer= layername[1])
 #make a function to convert gml into R and can return a data frame
 checkgml <- function(x){
-  png(file= paste("quad_checking/figures/", x, ".png", sep=""))
-  adair <- readOGR(dsn= filename, layer= layername[1])
-  plot(x[,c('x', 'y')], col = 'red', pch=19, cex = 0.5, main = x)
+  layername <- ogrListLayers(x)
+  
+  map_name <- substr(x, 
+                     max(gregexpr('/', x, fixed=TRUE)[[1]]) + 1, 
+                     max(gregexpr('.', x, fixed=TRUE)[[1]]) - 1)
+  
+  map <- readOGR(dsn= x, layer= layername[1])
+  data.frame(lgdist.test = map@data$diam1 > 500 & map@data$diam2 > 500 
+             & map@data$diam3 > 500 & map@data$diam4 > 500)
+  data.frame(
+
+  )
+  png(file= paste0("figures/", map_name, ".png"))
+  plot(map, col = 'red', pch=19, cex = 0.5, main = map_name)
   trash <- dev.off()
 }
-
+apply(map,checkgml)
+??FUN
 
