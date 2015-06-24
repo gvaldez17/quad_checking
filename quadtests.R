d0406431230f0ce21bc1bfea9958c5dc8e40d25b
Simon <goring@wisc.edu>
2015-06-24 17:26:13 -0500

    wrote test to check if trees one and two are exact twins

--- quadtests.R
+++ quadtests.R
@@ -4,7 +4,7 @@ data.frame(az.test = adair@data$az1 > 90, diam.test = is.na(adair@data$diam1) & 
 data.frame(val.test = adair@data$species1 > 0 & adair@data$species2 > 0)
 #test to see if tree one has no value (0) while tree two has a value (distance)
 data.frame(nodist.test = adair@data$dist1==0 & adair@data$dist2 > 0)
-#test to see if two trees have same data..how to write: do any two trees have the same data all across the board?
+#test to see if two trees have same data(diameters)..how to write: do any two trees have the same data all across the board?
 data.frame(twin.test = adair@data$diam1 == adair@data$diam2)
 #test to see if a tree hass too small of a diameter (what would too small be?). is 1 okay to use?
 data.frame(small.test = adair@data$diam1 < 1 & adair@data$diam2 < 1 & adair@data$diam3 < 1 & adair@data$diam4 < 1)
@@ -17,4 +17,8 @@ data.frame(nodiam.test = map@data$diam1==0 & map@data$diam2 > 0)
 #test to see if ree one has no value (0) while tree two has a value (azimuth)
 dat.frame(noaz.test = map@data$az1==0 & map@data$az2 >0)
 #test to see if both trees have a distance greater than 75
-1&2lgdist.test = map@data$dist1 > 75 & map@data$dist2 > 75
+one&2lgdist.test = map@data$dist1 > 75 & map@data$dist2 > 75
+#test to see if two trees have theexact same data
+exacttwin.test = map@data$dist1 == map@data$dist2 & 
+                 map@data$diam1 == map@data$diam2 & 
+                 map@data$az1 == map@data$az2)
