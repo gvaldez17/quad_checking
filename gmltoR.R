7b36e7151b169ae35d52e4fed36f5fbc611469aa
Simon <goring@wisc.edu>
2015-06-25 14:28:11 -0500

    changed the ands to ors in the large distance test and the small diameter test

--- gmltoR.R
+++ gmltoR.R
@@ -30,15 +30,15 @@ checkgml <- function(x){
   #making a dataframe which includes the map name and x and y coordinates
   geo <- data.frame(map_name,coordinates(map))
   #making a dataframe which includes column names for each test
-  tests <- data.frame(lgdist = map@data$dist1 > 500 & map@data$dist2 > 500 &
-                                      map@data$dist3 > 500 & map@data$dist4 > 500,
+  tests <- data.frame(lgdist = map@data$dist1 > 500 | map@data$dist2 > 500 |
+                                      map@data$dist3 > 500 | map@data$dist4 > 500,
                       nodist = map@data$dist1==0 & map@data$dist2 > 0,
                       az = map@data$az1 > 90, 
                       diam.test = is.na(map@data$diam1) & !is.na(map@data$diam2),
                       val = is.na(map@data$species1) & !is.na(map@data$species2),
                       twin = map@data$diam1 == map@data$diam2,
-                      smdiam = map@data$diam1 < 1 & map@data$diam2 < 1 & 
-                                     map@data$diam3 < 1 & map@data$diam4 < 1,
+                      smdiam = map@data$diam1 < 1 | map@data$diam2 < 1 | 
+                                     map@data$diam3 < 1 | map@data$diam4 < 1,
                       lgdiam = map@data$diam1 > 60 & map@data$diam2 > 60 &
                                      map@data$diam3 > 60 & map@data$diam4 > 60,
                       nodiam = (map@data$diam1==0 | is.na(map@data$diam1)) & map@data$diam2 > 0,
